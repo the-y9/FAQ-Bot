d56dedafc2d599dff924596c4eadcaf210a99b32
Yash Mishra <21f1006461@ds.study.iitm.ac.in>
2025-06-08 23:43:01 +0530

    graphing at client

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import importlib
 import sys
 import os
 v = 'v2'
-v1_path = os.path.join(os.path.dirname(__file__), v)
-sys.path.append(v1_path)
+v_path = os.path.join(os.path.dirname(__file__), v)
+sys.path.append(v_path)
 
 # Dynamically import the module
 v_main = importlib.import_module(f'{v}.main')
 
 if hasattr(v_main, 'main'):
     v_main.main()
+
